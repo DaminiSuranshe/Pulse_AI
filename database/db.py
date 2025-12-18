2b2c239d046ae5abbae78035510c9ca88de8ea05
Damini <daminisuranshe29@gmail.com>
2025-12-18 11:13:33 +0530

    modified:   requirements.txt

--- database/db.py
+++ database/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/ayurveda_ai"
+DATABASE_URL = "postgresql://postgres:0511@localhost:5432/ayurveda_ai"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
